6b2cd695109abbe3529776dbd6f71eb72401160b
Mohamed Yasser <mohamed3laily@gmail.com>
2025-10-08 08:21:36 +0300

    stage 2

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,18 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
-  connection.write(`+PONG\r\n`);
+  connection.setKeepAlive(true);
+
+  connection.on("data", (data: Buffer) => {
+    console.log("Received data:", data.toString());
+    connection.write("+PONG\r\n");
+  });
+
+  connection.on("error", (err) => {
+    console.error("Connection error:", err);
+  });
 });
 
 server.listen(6379, "127.0.0.1");
