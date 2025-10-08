b218458db126ed3856df898ac49b851767eb88a9
Mohamed Yasser <mohamed3laily@gmail.com>
2025-10-08 21:47:14 +0300

    feat: added ttl for the store

--- app/handler/commands/set.ts
+++ app/handler/commands/set.ts
@@ -5,7 +5,41 @@ export function set(args: string[]): string {
   if (args.length < 2) {
     return encodeError("ERR wrong number of arguments for 'SET'");
   }
-  const [key, value] = args;
-  setValue(key, value);
+
+  const [key, value, ...options] = args;
+  const ttlMs = parseTtlOptions(options);
+
+  if (ttlMs instanceof Error) {
+    return encodeError(ttlMs.message);
+  }
+
+  setValue(key, value, ttlMs);
   return encodeSimpleString("OK");
+}
+
+function parseTtlOptions(options: string[]): number | undefined | Error {
+  if (options.length === 0) return undefined;
+
+  let ttlMs: number | undefined;
+
+  for (let i = 0; i < options.length; i += 2) {
+    const option = options[i]?.toUpperCase();
+    const val = options[i + 1];
+    if (!option || !val) return new Error("ERR syntax error");
+
+    const parsed = parseInt(val, 10);
+    if (isNaN(parsed)) {
+      return new Error("ERR value is not an integer or out of range");
+    }
+
+    if (option === "EX") {
+      ttlMs = parsed * 1000;
+    } else if (option === "PX") {
+      ttlMs = parsed;
+    } else {
+      return new Error("ERR syntax error");
+    }
+  }
+
+  return ttlMs;
 }
--- app/handler/store.ts
+++ app/handler/store.ts
@@ -1,15 +1,31 @@
-const store = new Map<string, string>();
+type Entry = {
+  value: string;
+  expiresAt?: number; // in ms
+};
 
-export function setValue(key: string, value: string) {
-  store.set(key, value);
+const store = new Map<string, Entry>();
+
+export function setValue(key: string, value: string, ttlMs?: number) {
+  const expiresAt = ttlMs ? Date.now() + ttlMs : undefined;
+  store.set(key, { value, expiresAt });
 }
 
 export function getValue(key: string): string | undefined {
-  return store.get(key);
+  const entry = store.get(key);
+
+  if (!entry) return undefined;
+
+  if (entry.expiresAt && entry.expiresAt < Date.now()) {
+    store.delete(key);
+    return undefined;
+  }
+
+  return entry.value;
 }
 
 export function hasKey(key: string): boolean {
-  return store.has(key);
+  const val = getValue(key);
+  return val !== undefined;
 }
 
 export function clearStore() {
